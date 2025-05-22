5d36db76b40316a77fc91a7341dc7df165193123
hafidzahm <hafizhahm123@gmail.com>
2025-05-22 13:59:26 +0700

    chore: adding configuring AuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import OnboardingPage from "./pages/OnboardingPage";
+import AuthLayout from "./layouts/AuthLayout";
+import DashboardPage from "./pages/DashboardPage";
+import TriviaPage from "./pages/TriviaPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<OnboardingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/trivia/:triviaId" element={<TriviaPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
