9cc11d47ac78748347bade81ee4fa6c8a98fa2f8
hafidzahm <hafizhahm123@gmail.com>
2025-05-24 13:18:56 +0700

    chore: redesiging card

--- src/pages/TriviaPage.jsx
+++ src/pages/TriviaPage.jsx
@@ -21,7 +21,7 @@ export default function TriviaPage() {
     }
   }
   return (
-    <div className="w-full min-h-screen bg-amber-200 p-5 flex flex-col justify-center items-center">
+    <div className="w-full min-h-screen bg-amber-200 p-5 flex flex-col justify-start items-center">
       <CardQuestion question={quiz?.question} />
     </div>
   );
@@ -30,33 +30,34 @@ export default function TriviaPage() {
 function CardQuestion({ question }) {
   return (
     <div className="block max-w-sm min-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <div className={`min-h-[200px]`}>
+      <div className={`min-h-[70vh]`}>
         {" "}
         {question ? (
           <>
-            <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
               {question}
             </h5>
           </>
         ) : (
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            loading...
+            ...
           </h5>
         )}
       </div>
-
-      <button
-        type="button"
-        className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-base px-10 py-5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-      >
-        True
-      </button>
-      <button
-        type="button"
-        className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-base px-10 py-5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-      >
-        False
-      </button>
+      <div className="flex flex-col">
+        <button
+          type="button"
+          className="text-white font-bold bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 rounded-full text-base px-12 py-6 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          True
+        </button>
+        <button
+          type="button"
+          className="text-white font-bold bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 rounded-full text-base px-12 py-6 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          False
+        </button>
+      </div>
     </div>
   );
 }
