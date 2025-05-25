095188c4d826051121651c56f2cb38f13a6149b7
hafidzahm <hafizhahm123@gmail.com>
2025-05-25 16:03:32 +0700

    refactor: login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
     }
   }
   return (
-    <div className="font-montserrat bg-amber-100 min-h-screen flex flex-col justify-center p-5">
+    <div className="font-montserrat min-h-screen flex flex-col justify-center p-5">
       <LoginDialog
         username={username}
         setUsername={inputUsername}
@@ -52,14 +52,14 @@ function LoginDialog({
   submitData,
 }) {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 rounded-2xl">
+    <section className="bg-gray-50 border border-gray-500 bg-gray-300 rounded-2xl">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
           Quizzz!
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Sign in to your account
             </h1>
             <form
@@ -70,7 +70,7 @@ function LoginDialog({
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Username
                 </label>
@@ -80,7 +80,7 @@ function LoginDialog({
                   id="username"
                   value={username}
                   onChange={setUsername}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="johndoe123"
                   required=""
                 />
@@ -88,7 +88,7 @@ function LoginDialog({
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Password
                 </label>
@@ -99,14 +99,14 @@ function LoginDialog({
                   value={password}
                   onChange={setPassword}
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   required=""
                 />
               </div>
 
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full bg-gray-300 bg-primary-600 border border-gray-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
